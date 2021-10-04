e81f298cc362d7c6d9d7aefd8cf826107dab2276
Nyola Mike <nyolamike@live.com>
2021-10-04 15:22:07 +0300

    esc6 creating a class

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Tree from './lib/Tree';
 
 function App() {
+
+  //creating an objbect from a class
+  var mikesTrees = new Tree("mTree1",0.0011334,45.7688,"UG","Nalumunye, Jomayi Estates, Wakiso");
+  console.log("mikesTrees: ", mikesTrees);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> <img src={logo}  /> Pa Yo Trees</h1>
+      <p>
+        Your trees, your climate, our environment, our future
+      </p>
     </div>
   );
 }
---
+++ src/lib/Tree.js
@@ -0,0 +1,11 @@
+class Tree {
+    constructor(name,latitude,longitude,countryCode,locationName){
+        this.name = name
+        this.latitude = latitude
+        this.longitude = longitude
+        this.countryCode = countryCode
+        this.locationName = locationName
+    }
+}
+
+export default Tree
